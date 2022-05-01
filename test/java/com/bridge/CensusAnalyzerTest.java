330d2a6ce49943b6ce524e53044ece9086f296c8
Karthik <jeefkarthik036@gmail.com>
2022-05-01 22:02:00 +0530

    loading info from CSV

---
+++ test/java/com/bridge/CensusAnalyzerTest.java
@@ -0,0 +1,20 @@
+package com.bridge;
+
+import com.bridge.model.CensusAnalyzer;
+import org.apache.commons.lang3.builder.ToStringExclude;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.io.IOException;
+
+public class CensusAnalyzerTest {
+    private String INDIAN_CENSUS_CSV_FILE_PATH = "./src/main/resources/IndiaStateCensusData.csv";
+
+    @Test
+    public void givenIndianCensusCSVFile_WhenLoad_ShouldReturnCorrectRecords() throws IOException {
+
+        CensusAnalyzer censusAnalyzer = new CensusAnalyzer();
+        int count = censusAnalyzer.loadIndiaCensusData(INDIAN_CENSUS_CSV_FILE_PATH);
+        Assert.assertEquals(29,count);
+    }
+}
